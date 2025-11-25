2494afd37064bcf90870f24fee3b9ada641960b6
ChrisMcElroy <csmcelroy@me.com>
2025-11-25 16:07:22 -0500

    Initial Commit

---
+++ rules/rulesApp.swift
@@ -0,0 +1,17 @@
+//
+//  rulesApp.swift
+//  rules
+//
+//  Created by 4 on '25.11.25.
+//
+
+import SwiftUI
+
+@main
+struct rulesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
